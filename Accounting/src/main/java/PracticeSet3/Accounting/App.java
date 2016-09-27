681ac2f45f99834c797ae2f87167ce8eb85ea215
DavidPennington <DavidPennington@wifi-roaming-128-4-94-165.host.udel.edu>
2016-09-27 13:37:52 -0400

    commit 1

---
+++ Accounting/src/main/java/PracticeSet3/Accounting/App.java
@@ -0,0 +1,13 @@
+package PracticeSet3.Accounting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Accounting/src/test/java/PracticeSet3/Accounting/Account_Test.java
@@ -0,0 +1,9 @@
+package PracticeSet3.Accounting;
+
+public class Account_Test {
+	Account acct1 = new Account();
+	acct1.setId(1122);
+	acct1.setBalance(22000);
+	
+	
+}
